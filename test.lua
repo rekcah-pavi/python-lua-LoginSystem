4b87ab1ffc1699aeafda5d787adcb572abd8c6f4
rekcah-pavi <55050467+rekcah-pavi@users.noreply.github.com>
2022-03-10 10:51:16 +0530

    Create test.lua

---
+++ test.lua
@@ -0,0 +1,48 @@
+
+server = "your website"
+
+c = gg.makeRequest(server.."/mykey").content
+
+ch = gg.prompt(
+	{'Enter your key and pres ok to continue'},
+	{[1] = c},
+	{[1] = 'text'})
+
+if ch == nill then
+ gg.alert("Please enter key and press ok")
+   os.exit()
+end
+
+pass = ch[1]
+
+str = pass
+if string.find(str, "/") then
+  gg.alert("Invlied Key")
+  os.exit()
+end
+
+if pass == "" then
+      x = gg.alert("Wrong key\n Get key form here \n\n https://mrgroot.herokuapp.com","copy link")
+   if x == 1 then 
+      gg.copyText(server)
+   end
+   os.exit()
+end
+
+a = gg.makeRequest(server.."/login/"..pass.."").content
+
+if a == "true" then
+   gg.alert("Login Success")
+elseif a =="false" then
+   x = gg.alert("Wrong key\n Get key form here \n\n "..server,"copy link")
+   if x == 1 then 
+      gg.copyText(server)
+   end
+   os.exit()
+else
+  gg.alert("No response form server/No internet Connection")
+  os.exit()
+end
+
+
+
